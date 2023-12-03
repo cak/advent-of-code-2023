2976a02de7b6e5c571a60d311e8c117f9ee2f690
cak <cak@fastmail.com>
2023-12-03 06:33:43 -0500

    Day 3 - Part 1

---
+++ src/Day03.kt
@@ -0,0 +1,70 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val axes = input
+                .flatMapIndexed { yi, y ->
+                    y.toCharArray()
+                            .mapIndexed { xi, x ->
+                                if (!x.isDigit() && x != '.') {
+                                    Pair(xi, yi)
+                                } else {
+                                    null
+                                }
+                            }
+                }.filterNotNull()
+
+        val solution = input
+                .flatMapIndexed { yi, y ->
+                    Regex("(\\d+)").findAll(y).toList().filter { m ->
+                        m.range.any { xi -> matchAxes(axes, xi, yi) }
+                    }.mapNotNull { it.value.toIntOrNull() }
+                }
+
+        return solution.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val axes = input
+                .flatMapIndexed { yi, y ->
+                    y.toCharArray()
+                            .mapIndexed { xi, x ->
+                                if (!x.isDigit() && x != '.') {
+                                    Pair(xi, yi)
+                                } else {
+                                    null
+                                }
+                            }
+                }.filterNotNull()
+
+        val solution = input
+                .flatMapIndexed { yi, y ->
+                    Regex("(\\d+)").findAll(y).toList().filter { m ->
+                        m.range.any { xi -> matchAxes(axes, xi, yi) }
+                    }.mapNotNull { it.value.toIntOrNull() }
+                }
+
+        return solution.sum()
+    }
+
+// test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 4361)
+// check(part2(testInput) == 1)
+
+    val input = readInput("Day03")
+    part1(input).println()
+//  part2(input).println()
+}
+
+
+fun matchAxes(axes: List<Pair<Int, Int>>, xi: Int, yi: Int) =
+        axes.contains(Pair(xi - 1, yi))
+                || axes.contains(Pair(xi + 1, yi))
+
+                || axes.contains(Pair(xi, yi - 1))
+                || axes.contains(Pair(xi, yi + 1))
+
+                || axes.contains(Pair(xi + 1, yi + 1))
+                || axes.contains(Pair(xi + 1, yi - 1))
+
+                || axes.contains(Pair(xi - 1, yi - 1))
+                || axes.contains(Pair(xi - 1, yi + 1))
