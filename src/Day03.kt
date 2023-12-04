0acfd000318c4a8dbdf3b3c1162a4b5adeb2fbbb
cak <cak@fastmail.com>
2023-12-03 20:29:57 -0500

    Day03 - Part II

--- src/Day03.kt
+++ src/Day03.kt
@@ -36,25 +36,51 @@ fun main() {
                 }.filterNotNull()
 
         val solution = input
+                .asSequence()
                 .flatMapIndexed { yi, y ->
-                    Regex("(\\d+)").findAll(y).toList().filter { m ->
-                        m.range.any { xi -> matchAxes(axes, xi, yi) }
-                    }.mapNotNull { it.value.toIntOrNull() }
+                    Regex("(\\d+)").findAll(y).toList().flatMap { m ->
+                        m.range.mapNotNull { xi -> findMatchAxes(axes, xi, yi) }.map {
+                            Pair(it, m.value.toInt())
+                        }.distinct()
+                    }
+                }.groupBy { it.first }.toList().map { it.second.map { s -> s.second } }
+                .filter { it.size >= 2 }.sumOf { g ->
+                    g.reduce { acc, i -> acc * i }
                 }
 
-        return solution.sum()
+        return solution
     }
+
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 4361)
-// check(part2(testInput) == 1)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
-//  part2(input).println()
+    part2(input).println()
 }
 
+
+fun findMatchAxes(axes: List<Pair<Int, Int>>, xi: Int, yi: Int) =
+        axes.find {
+            it == Pair(xi - 1, yi)
+        } ?: axes.find {
+            it == Pair(xi + 1, yi)
+        } ?: axes.find {
+            it == Pair(xi, yi - 1)
+        } ?: axes.find {
+            it == Pair(xi, yi + 1)
+        } ?: axes.find {
+            it == Pair(xi + 1, yi + 1)
+        } ?: axes.find {
+            it == Pair(xi + 1, yi - 1)
+        } ?: axes.find {
+            it == Pair(xi - 1, yi - 1)
+        } ?: axes.find {
+            it == Pair(xi - 1, yi + 1)
+        }
 
 fun matchAxes(axes: List<Pair<Int, Int>>, xi: Int, yi: Int) =
         axes.contains(Pair(xi - 1, yi))
