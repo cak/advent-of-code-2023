137a7586029ec96bf1f0d6d4b75a49a6deb8fb05
cak <cak@fastmail.com>
2023-12-09 08:53:22 -0500

    day 5 progress

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,33 +1,47 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
-fun main() {
-    fun part1(input: String): Int {
+suspend fun main() {
+    suspend fun part1(input: String): Long {
         val maps = input.split("\n\n")
-            .map { m -> m.split(":")}
-  
+                .map { m -> m.split(":") }
+
         val seeds = maps.take(1).last().last()
-            .split(" ")
-            .mapNotNull { it.trim().toIntOrNull() }
-        
-        println(seeds)
+                .split(" ")
+                .mapNotNull { it.trim().toLongOrNull() }
 
-        val sourceDest = maps.drop(1).map {
-            it.first() to it.last().split("\n")
-                .map { it.split(" ")
-                    .mapNotNull {
-                        it.trim().toIntOrNull()
+        val sourceDest = maps
+                .drop(1).map {
+                    it.first() to it.last().split("\n")
+                            .map { e ->
+                                e.split(" ")
+                                        .mapNotNull { t ->
+                                            t.trim().toLongOrNull()
+                                        }
+                            }
+                }.map { l ->
+                    Pair(l.first,
+                            l.second.drop(1)
+                                    .map { w -> listOf(w[0] until (w[0] + w[2]), w[1] until (w[1] + w[2])) }
+                                    .map { r -> Pair(r.first(), r.last()) })
+                }
+
+        val solution = seeds.asFlow().map { s ->
+            sourceDest.fold(s) { d, m ->
+                m.second.asFlow().map { (dest, src) ->
+                    val index = src.toList().binarySearch(d)
+                    if (index >= 0) {
+                        dest.elementAt(src.indexOf(d))
+                    } else {
+                        d
                     }
-            }}.map { l ->
-                Pair(l.first,
-                     l.second.drop(1)
-                         .map { w -> listOf(w[0] .. (w[0] + w[2]), w[1] .. (w[1] + w[2]))})
-                
-
+                }.firstOrNull { it != d } ?: d
             }
-            .forEach { println(it) }
-        
-        return 1
+        }.toList().minOf { it }
+
+        return solution
     }
 
     fun part2(input: List<String>): Int {
@@ -35,11 +49,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput= Path("input/Day05_test.txt").readText()
-    check(part1(testInput) == 1)
+    val testInput = Path("input/Day05_test.txt").readText()
+    check(part1(testInput) == 35.toLong())
     // check(part2(testInput) == 1)
 
-    val input = Path("input/Day05_test.txt").readText()
-   // part1(input).println()
+    val input = Path("input/Day05.txt").readText()
+    part1(input).println()
     //  part2(input).println()
 }
